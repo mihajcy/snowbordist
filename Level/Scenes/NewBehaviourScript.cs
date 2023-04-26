68ac1e9860f4829cb08acdfc4fc9316fe35384e6
mihajcy <120049276+mihajcy@users.noreply.github.com>
2023-04-26 22:12:38 +0500

    Add files via upload

---
+++ Level/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+   public void loadSceneZero()
+   {
+    SceneManager.LoadScene(0);
+   }
+   public void loadSceneOne()
+   {
+    SceneManager.LoadScene(1);
+   }
+   public void loadSceneTwo()
+   {
+    SceneManager.LoadScene(2);
+   }
+   public void loadSceneThree()
+   {
+    SceneManager.LoadScene(3);
+   }
+   public void loadSceneFour()
+   {
+    SceneManager.LoadScene(4);
+   }
+   public void loadSceneFive()
+   {
+    SceneManager.LoadScene(5);
+   }
+}
